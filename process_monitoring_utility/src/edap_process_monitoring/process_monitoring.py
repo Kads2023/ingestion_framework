fc8586317d5f401c0f526a5e2bb946b2501b2c3e
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-05-08 12:31:53 +0100

    Update process_monitoring.py

--- process_monitoring_utility/src/edap_process_monitoring/process_monitoring.py
+++ process_monitoring_utility/src/edap_process_monitoring/process_monitoring.py
@@ -122,6 +122,9 @@ class ProcessMonitoring:
         return [param_dict[key] for key in param_order]
 
     def get_and_set_job_id(self, raise_exception=False):
+        """
+        Retrieves and sets the job ID from the job details table based on provided parameters.
+        """
         this_module = f"[{self.this_class_name}.get_and_set_job_id()] -"
         job_details_table_name = self.job_args_obj.get("job_details_table_name")
         load_type = self.job_args_obj.get("load_type")
@@ -129,34 +132,30 @@ class ProcessMonitoring:
         source = self.job_args_obj.get("source")
         source_type = self.job_args_obj.get("source_type")
         query_to_execute = (
-            f"SELECT "
-            f"job_id "
-            f"FROM "
-            f"{job_details_table_name} "
-            f"WHERE "
-            f"source_system = '{source_system}' AND "
-            f"source = '{source}' AND "
-            f"source_type = '{source_type}' AND "
-            f"load_type = '{load_type}'"
+            f"SELECT job_id FROM {job_details_table_name} WHERE source_system = :source_system AND source = :source AND source_type = :source_type AND load_type = :load_type"
         )
-        job_details = self.execute_query_and_get_results(query_to_execute)
+        param_dict = {
+            "source_system": source_system,
+            "source": source,
+            "source_type": source_type,
+            "load_type": load_type,
+        }
+        job_details = self.execute_query_and_get_results(query_to_execute, param_dict=param_dict)
         job_details_count = job_details.count()
         job_details_list = job_details.collect()
         if job_details_count == 1:
             job_id = job_details_list[0]["job_id"]
-            self.job_args_obj.set("jo_id", job_id)
+            self.job_args_obj.set("job_id", job_id)
         else:
-            error_msg = (
-                f"{this_module} "
-                f"job_details_count --> {job_details_count}, "
-                f"job_details_count != 1, "
-                f"job_details_list --> {job_details_list}"
-            )
+            error_msg = f"{this_module} job_details_count --> {job_details_count}, job_details_count != 1, job_details_list --> {job_details_list}"
             self.lc.logger.error(error_msg)
             if raise_exception:
                 raise Exception(error_msg)
 
     def insert_job_details(self):
+        """
+        Inserts job details into the job details table.
+        """
         this_module = f"[{self.this_class_name}.insert_job_details()] -"
         job_details_table_name = self.job_args_obj.get("job_details_table_name")
         env = self.job_args_obj.get("env")
@@ -168,22 +167,26 @@ class ProcessMonitoring:
         source_type = self.job_args_obj.get("source_type")
         now_current_time = self.common_utils.get_current_time()
         query_to_execute = (
-            f"INSERT INTO "
-            f"{job_details_table_name} "
-            f"(_created, _modified, "
-            f"env, job_name, "
-            f"frequency, load_type, "
-            f"source_system, source, source_type) "
-            f"VALUES("
-            f"CAST('{now_current_time}' AS DATETIME), "
-            f"CAST('{now_current_time}' AS DATETIME), "
-            f"'{env}', '{job_name}', "
-            f"'{frequency}', '{load_type}', "
-            f"'{source_system}', '{source}', '{source_type}')"
+            f"INSERT INTO {job_details_table_name} (_created, _modified, env, job_name, frequency, load_type, source_system, source, source_type) "
+            f"VALUES(CAST(:_created AS DATETIME), CAST(:_modified AS DATETIME), :env, :job_name, :frequency, :load_type, :source_system, :source, :source_type)"
         )
-        self.execute_query_and_get_results(query_to_execute, fetch_results=False)
+        param_dict = {
+            "_created": now_current_time,
+            "_modified": now_current_time,
+            "env": env,
+            "job_name": job_name,
+            "frequency": frequency,
+            "load_type": load_type,
+            "source_system": source_system,
+            "source": source,
+            "source_type": source_type,
+        }
+        self.execute_query_and_get_results(query_to_execute, param_dict=param_dict, fetch_results=False)
 
     def check_and_get_job_id(self):
+        """
+        Checks and retrieves the job ID if not already set.
+        """
         this_module = f"[{self.this_class_name}.check_and_get_job_id()] -"
         self.get_and_set_job_id()
         job_id = self.job_args_obj.get("job_id")
@@ -192,22 +195,18 @@ class ProcessMonitoring:
             self.get_and_set_job_id(raise_exception=True)
 
     def check_already_processed(self, passed_job_id="", passed_run_date=""):
-        this_module = f"[{self.this_class_name}.insert_job_details()] -"
+        """
+        Checks if a job has already been processed based on job ID and run date.
+        """
+        this_module = f"[{self.this_class_name}.check_already_processed()] -"
         job_run_details_table_name = self.job_args_obj.get("job_run_details_table_name")
         self.common_utils.validate_function_param(
             this_module,
             {
-                "passed_job_id": {
-                    "input_value": passed_job_id,
-                    "data_type": "str",
-                },
-                "passed_run_date": {
-                    "input_value": passed_run_date,
-                    "data_type": "str",
-                }
-            }
+                "passed_job_id": {"input_value": passed_job_id, "data_type": "str"},
+                "passed_run_date": {"input_value": passed_run_date, "data_type": "str"},
+            },
         )
-
         if passed_job_id:
             job_id = passed_job_id
         else:
@@ -217,109 +216,98 @@ class ProcessMonitoring:
             run_date = passed_run_date
         else:
             run_date = self.job_args_obj.get("run_date")
+        
         query_to_execute = (
-            f"SELECT "
-            f"job_id, run_status, run_error_detail "
-            f"FROM "
-            f"{job_run_details_table_name} "
-            f"WHERE "
-            f"job_id = {job_id} AND "
-            f"run_date = '{run_date}' AND "
-            f"UPPER(run_status) = 'COMPLETED'"
+            f"SELECT job_id, run_status, run_error_detail FROM {job_run_details_table_name} WHERE job_id = :job_id AND run_date = :run_date AND UPPER(run_status) = 'COMPLETED'"
         )
-        job_completed_details = self.execute_query_and_get_results(query_to_execute)
+        param_dict = {"job_id": job_id, "run_date": run_date}
+        job_completed_details = self.execute_query_and_get_results(query_to_execute, param_dict=param_dict)
+        
         job_already_completed = False
         if job_completed_details.count() == 1:
             job_already_completed = True
         self.job_args_obj.set(f"{job_id}_completed", job_already_completed)
 
     def get_and_set_run_id(self):
+        """
+        Retrieves and sets the run ID for the current job run.
+        """
         this_module = f"[{self.this_class_name}.get_and_set_run_id()] -"
         job_run_details_table_name = self.job_args_obj.get("job_run_details_table_name")
         job_id = self.job_args_obj.get("job_id")
         run_date = self.job_args_obj.get("run_date")
         run_start_time = self.job_args_obj.get("run_start_time")
+        
         query_to_execute = (
-            f"SELECT "
-            f"run_id "
-            f"FROM "
-            f"{job_run_details_table_name} "
-            f"WHERE "
-            f"job_id = {job_id} AND "
-            f"run_start_time = CAST('{run_start_time}' AS DATETIME) AND "
-            f"run_date = '{run_date}'"
+            f"SELECT run_id FROM {job_run_details_table_name} WHERE job_id = :job_id AND run_start_time = CAST(:run_start_time AS DATETIME) AND run_date = :run_date"
         )
-        run_details = self.execute_query_and_get_results(query_to_execute)
+        param_dict = {"job_id": job_id, "run_start_time": run_start_time, "run_date": run_date}
+        run_details = self.execute_query_and_get_results(query_to_execute, param_dict=param_dict)
         run_details_count = run_details.count()
         run_details_list = run_details.collect()
+        
         if run_details_count == 1:
             run_id = self.job_args_obj.get("run_id")
             self.job_args_obj.set("run_id", run_id)
         else:
             error_msg = (
-                f"{this_module} "
-                f"run_details_count --> {run_details_count}, "
-                f"run_details_count != 1, "
-                f"run_details_list --> {run_details_list}"
+                f"{this_module} run_details_count --> {run_details_count}, run_details_count != 1, run_details_list --> {run_details_list}"
             )
             self.lc.logger.error(error_msg)
             raise Exception(error_msg)
 
     def insert_update_job_run_status(self, passed_status, passed_comments=""):
+        """
+        Inserts or updates the job run status in the job run details table.
+        """
         this_module = f"[{self.this_class_name}.insert_update_job_run_status()] -"
         self.common_utils.validate_function_param(
             this_module,
             {
-                "passed_status": {
-                    "input_value": passed_status,
-                    "data_type": "str",
-                    "check_empty": True,
-                },
-                "passed_comments": {
-                    "input_value": passed_comments,
-                    "data_type": "str",
-                }
-            }
+                "passed_status": {"input_value": passed_status, "data_type": "str", "check_empty": True},
+                "passed_comments": {"input_value": passed_comments, "data_type": "str"},
+            },
         )
-
         job_run_details_table_name = self.job_args_obj.get("job_run_details_table_name")
         job_id = self.job_args_obj.get("job_id")
         run_id = self.job_args_obj.get("run_id")
         run_date = self.job_args_obj.get("run_date")
         run_start_time = self.job_args_obj.get("run_start_time")
-        run_row_count = self.job_args_obj.get("run_row_count")
-        if run_row_count == "":
-            run_row_count = 0
+        run_row_count = self.job_args_obj.get("run_row_count") or 0
         now_current_time = self.common_utils.get_current_time()
+        
         if run_id == "":
             query_to_execute = (
-                f"INSERT INTO "
-                f"{job_run_details_table_name} "
-                f"(job_id, _created, _modified, "
-                f"run_start_time, run_end_time, "
-                f"run_date, run_row_count, "
-                f"run_status, run_error_detail) "
-                f"VALUES({job_id}, "
-                f"CAST('{now_current_time}' AS DATETIME), "
-                f"CAST('{now_current_time}' AS DATETIME), "
-                f"CAST('{run_start_time}' AS DATETIME), "
-                f"CAST('{now_current_time}' AS DATETIME), "
-                f"CAST('{run_date}' AS DATE), {run_row_count}, "
-                f"'{passed_status}', '{passed_comments}')"
+                f"INSERT INTO {job_run_details_table_name} "
+                f"(job_id, _created, _modified, run_start_time, run_end_time, run_date, run_row_count, run_status, run_error_detail) "
+                f"VALUES(:job_id, CAST(:_created AS DATETIME), CAST(:_modified AS DATETIME), CAST(:run_start_time AS DATETIME), CAST(:run_end_time AS DATETIME), CAST(:run_date AS DATE), :run_row_count, :run_status, :run_error_detail)"
             )
+            param_dict = {
+                "job_id": job_id,
+                "_created": now_current_time,
+                "_modified": now_current_time,
+                "run_start_time": run_start_time,
+                "run_end_time": now_current_time,
+                "run_date": run_date,
+                "run_row_count": run_row_count,
+                "run_status": passed_status,
+                "run_error_detail": passed_comments,
+            }
         else:
             query_to_execute = (
-                f"UPDATE "
-                f"{job_run_details_table_name} "
-                f"SET "
-                f"_modified=CAST('{now_current_time}' AS DATETIME), "
-                f"run_end_time=CAST('{now_current_time}' AS DATETIME), "
-                f"run_row_count={run_row_count}, "
-                f"run_status='{passed_status}', "
-                f"run_error_detail='{passed_comments}' "
-                f"WHERE "
-                f"run_id = {run_id} AND job_id = {job_id}"
+                f"UPDATE {job_run_details_table_name} SET _modified=CAST(:_modified AS DATETIME), run_end_time=CAST(:run_end_time AS DATETIME), "
+                f"run_row_count=:run_row_count, run_status=:run_status, run_error_detail=:run_error_detail WHERE run_id = :run_id AND job_id = :job_id"
             )
-        self.execute_query_and_get_results(query_to_execute, fetch_results=False)
+            param_dict = {
+                "_modified": now_current_time,
+                "run_end_time": now_current_time,
+                "run_row_count": run_row_count,
+                "run_status": passed_status,
+                "run_error_detail": passed_comments,
+                "run_id": run_id,
+                "job_id": job_id,
+            }
+
+        self.execute_query_and_get_results(query_to_execute, param_dict=param_dict, fetch_results=False)
         if run_id == "":
             self.get_and_set_run_id()
